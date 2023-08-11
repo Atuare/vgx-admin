8f3eee51f2367f1f7997f8d227cbc192de16a205
Iury <iurysena74@gmail.com>
2023-08-11 19:05:36 -0300

    :sparkles: feat: added sidebar menu on page layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,11 @@
+"use client";
+
 import { ReactNode } from "react";
+import { usePathname } from "next/navigation";
 import "./globals.scss";
 import type { Metadata } from "next";
 import { Sora } from "next/font/google";
+import { Menu } from "@/components/Menu";
 
 const sora = Sora({ subsets: ["latin"], weight: ["400", "600", "700"] });
 
@@ -11,9 +15,17 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
+  const pathname = usePathname();
+
   return (
     <html lang="pt-br">
-      <body className={sora.className}>{children}</body>
+      <body
+        className={sora.className}
+        style={{ display: pathname !== "/login" ? "flex" : "inherit" }}
+      >
+        {pathname !== "/login" && <Menu />}
+        {children}
+      </body>
     </html>
   );
 }
