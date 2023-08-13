d7c6347efe5b6784d83b2d4049bade5735820efc
Iury <iurysena74@gmail.com>
2023-08-13 09:01:20 -0300

    :recycle: refactor: changed normal input to SearchInput component - HomePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import dayjs from "dayjs";
 import "../lib/dayjs";
 import styles from "./Home.module.scss";
@@ -5,10 +6,17 @@ import { AdmProfile } from "@/components/AdmProfile";
 import { HomeData } from "@/components/HomeData";
 import Avatar from "@/assets/avatar.png";
 import { Calendar } from "@/components/Calendar";
+import { SearchInput } from "@/components/SearchInput";
+import { useState } from "react";
 
 export default function Home() {
+  const [value, setValue] = useState<string>("");
   const date = dayjs(new Date()).format("dddd, DD MMM YYYY");
   const actualDate = date.charAt(0).toUpperCase() + date.slice(1);
+
+  function handleInputValue(value: string) {
+    setValue(value);
+  }
 
   return (
     <div className={styles.home}>
@@ -19,7 +27,7 @@ export default function Home() {
         </div>
 
         <div className={styles.header__right}>
-          <input type="search" placeholder="Pesquisar..." />
+          <SearchInput handleChangeValue={handleInputValue} />
           <AdmProfile image={Avatar} name="Nome perfil" role="Função" />
         </div>
       </header>
