b6a09413af8e7df4016f4a7aa033f1d6fc198df2
NeptuneXDB <neptunex63@gmail.com>
2023-08-14 02:11:23 -0300

    🐛 fix: temp remove stylelint from lint-staged

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,6 +1,4 @@
 module.exports = {
-  "{src,apps,libs,test}/**/*.{ts,tsx,scss}": [
-    "eslint --fix",
-    "stylelint --fix",
-  ],
+  "{src,apps,libs,test}/**/*.{ts,tsx}": ["eslint --fix"],
+  // "{src,apps,libs,test}/**/*.scss": ["stylelint --fix"],
 };
