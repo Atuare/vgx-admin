559a2f131d3a72e00c6f836d1b6dbaaa55201fe6
NeptuneXDB <neptunex63@gmail.com>
2023-08-17 14:01:17 -0300

    :recycle: refactor: change process and statistics api URL to use BASE_URL in config

--- src/utils/process.ts
+++ src/utils/process.ts
@@ -1,25 +1,23 @@
 import axios from "axios";
+import { BASE_URL } from "./config";
 
 export async function getAllProcess(page: number, size: number) {
-  return await axios.get(
-    `${process.env.NEXT_PUBLIC_BASE_API_URL}/process/findAll`,
-    {
-      headers: {
-        "ngrok-skip-browser-warning": true,
-        "content-type": "application/json",
-        Authorization:
-          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjRkY2E0YmEzLTA5ZDUtNGY0ZS1hZTBhLTc2OTc4OWUyZjJlZiIsIm5hbWUiOiJpdXJ5MiIsInByb2ZpbGUiOiJFTVBMT1lFRSIsImVtcGxveWVlIjp7ImlkIjoiZThjMzMwYzUtNWVmNS00NDIyLTg4MDgtNzVjMzVlZDNiOTVjIn0sImlhdCI6MTY5MjEyMDg5OSwiZXhwIjoxNzIzNjU2ODk5fQ.JjY86nLMLoE-vPDqFVsMenoZblyHF1QDKhVR43QSBdM",
-      },
-      params: {
-        page,
-        size,
-      },
+  return await axios.get(`${BASE_URL}/process/findAll`, {
+    headers: {
+      "ngrok-skip-browser-warning": true,
+      "content-type": "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjRkY2E0YmEzLTA5ZDUtNGY0ZS1hZTBhLTc2OTc4OWUyZjJlZiIsIm5hbWUiOiJpdXJ5MiIsInByb2ZpbGUiOiJFTVBMT1lFRSIsImVtcGxveWVlIjp7ImlkIjoiZThjMzMwYzUtNWVmNS00NDIyLTg4MDgtNzVjMzVlZDNiOTVjIn0sImlhdCI6MTY5MjEyMDg5OSwiZXhwIjoxNzIzNjU2ODk5fQ.JjY86nLMLoE-vPDqFVsMenoZblyHF1QDKhVR43QSBdM",
     },
-  );
+    params: {
+      page,
+      size,
+    },
+  });
 }
 
 export async function deleteProcess(id: string | string[]) {
-  return axios.delete(`${process.env.NEXT_PUBLIC_BASE_API_URL}/process/${id}`, {
+  return axios.delete(`${BASE_URL}/process/${id}`, {
     headers: {
       "ngrok-skip-browser-warning": true,
       Authorization:
