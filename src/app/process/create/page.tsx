59c801d8ccd8896b216669d913ca208d90b61a45
Iury <iurysena74@gmail.com>
2023-08-18 05:00:46 -0300

    :sparkles: feat: update ProcessCreate screen

--- src/app/process/create/page.tsx
+++ src/app/process/create/page.tsx
@@ -7,10 +7,14 @@ import { StepOne } from "@/components/ProcessCreate/StepOne";
 export default function ProcessCreate() {
   const [step, setStep] = useState(1);
 
+  const handleTogglePage = (page: number) => {
+    setStep(page);
+  };
+
   return (
     <div className={styles.process}>
       <Stepper step={step} />
-      {step === 1 && <StepOne />}
+      {step === 1 && <StepOne handleTogglePage={handleTogglePage} />}
     </div>
   );
 }
--- src/components/ProcessCreate/StepOne.tsx
+++ src/components/ProcessCreate/StepOne.tsx
@@ -1,19 +1,137 @@
-import { ReactNode } from "react";
+import { ReactNode, useEffect, useState } from "react";
 import { Select } from "../Select";
 import styles from "./StepOne.module.scss";
+import { TipTap } from "../TipTap";
+import { LocalSelect } from "../LocalSelect";
+import {
+  useGetAllRolesQuery,
+  useGetAllUnitsQuery,
+} from "@/services/api/fetchApi";
+import { Radio } from "../Radio";
+import { NumberInput } from "../NumberInput";
+import { FileInput } from "../FileInput";
+import { Button } from "../Button";
+import { ArrowCircleRight } from "@/assets/Icons";
+import { useRouter } from "next/navigation";
 
-const unitOptions = ["Unidade 1", "Unidade 2", "Unidade 3"];
+export function StepOne({
+  handleTogglePage,
+}: {
+  handleTogglePage: (page: number) => void;
+}) {
+  const { back } = useRouter();
 
-export function StepOne() {
+  const [unitOptions, setUnitOptions] = useState<string[]>([]);
+  const [roleOptions, setRoleOptions] = useState<string[]>([]);
+
+  const { data, isSuccess } = useGetAllUnitsQuery({ page: 1, size: 9999 });
+  const { data: RoleData, isSuccess: RoleDataSucess } = useGetAllRolesQuery({
+    page: 1,
+    size: 9999,
+  });
+
+  function getObservations(content: any) {
+    // console.log(content);
+  }
+
+  function getLastMessage(content: any) {
+    // console.log(content);
+  }
+
+  function getUnit(value: string) {
+    // console.log(value);
+  }
+
+  function getRole(value: string) {
+    // console.log(value);
+  }
+
+  function onChangeRadio(value: boolean) {
+    // console.log(value);
+  }
+
+  function onChangeCandidates(value: number) {
+    // console.log(value);
+  }
+
+  function onChangeFile(file: File) {
+    // console.log(event.target.files[0]);
+  }
+
+  useEffect(() => {
+    if (isSuccess) {
+      const units = data?.units.map(unit => unit.unitName);
+      setUnitOptions(units ?? []);
+    }
+
+    if (RoleDataSucess) {
+      const roles = RoleData?.roles.map(role => role.roleText);
+      setRoleOptions(roles ?? []);
+    }
+  }, [isSuccess, RoleDataSucess]);
+
   return (
     <div className={styles.container}>
       <section className={styles.container__form}>
-        <div>
+        <div className={styles.container__form__first}>
           <DataInput name="Unidade/Site" required width="296px">
-            <Select placeholder="Selecione os locais" options={unitOptions} />
+            <Select
+              placeholder="Selecione os locais"
+              options={unitOptions}
+              onChange={getUnit}
+            />
+          </DataInput>
+          <DataInput name="Cargo" required width="448px">
+            <LocalSelect
+              placeholder="Selecione os locais"
+              options={roleOptions}
+              onChange={getRole}
+            />
+          </DataInput>
+          <DataInput name="Currículo" width="264px" required>
+            <Radio onChange={onChangeRadio} />
+          </DataInput>
+        </div>
+
+        <div className={styles.container__form__second}>
+          <DataInput name="Data inicial" width="224px" required>
+            <input type="date" />
+          </DataInput>
+          <DataInput name="Inscrições até" width="224px">
+            <input type="date" />
+          </DataInput>
+          <DataInput name="Limite de candidaturas" width="224px">
+            <NumberInput onChange={onChangeCandidates} />
+          </DataInput>
+          <DataInput name="Upload banner vaga" width="301px" required>
+            <FileInput onChange={onChangeFile} />
           </DataInput>
         </div>
       </section>
+
+      <div className={styles.container__editor}>
+        <h1>
+          Observações <span>(opcional)</span>
+        </h1>
+        <TipTap getContentFromEditor={getObservations} />
+      </div>
+
+      <div className={styles.container__editor}>
+        <h1>
+          Mensagem exibida ao final do cadastro <span>(opcional)</span>
+        </h1>
+        <TipTap getContentFromEditor={getLastMessage} />
+      </div>
+
+      <div className={styles.container__buttons}>
+        <Button buttonType="default" text="Cancelar" onClick={() => back()} />
+        <Button
+          buttonType="primary"
+          text="Próximo"
+          icon={<ArrowCircleRight />}
+          onClick={() => handleTogglePage(2)}
+        />
+      </div>
     </div>
   );
 }
@@ -35,6 +153,7 @@ function DataInput({
         {name}
         {required && <span>*</span>}
       </label>
+      <label htmlFor={name}>{!required && <p>(opcional)</p>}</label>
       {children}
     </div>
   );
