9bfcffcefdef0877e400a00421d2354d28f01469
Iury <iurysena74@gmail.com>
2023-08-18 04:54:58 -0300

    :sparkles: feat: uptade select component

--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -4,13 +4,19 @@ import { ChevronDown } from "@/assets/Icons";
 
 interface SelectProps {
   options: string[];
-  value?: string;
-  onChange?: (value: string) => void;
+  defaultValue?: string;
+  onChange: (value: string) => void;
   placeholder: string;
 }
 
-export function Select({ onChange, options, placeholder, value }: SelectProps) {
+export function Select({
+  onChange,
+  options,
+  placeholder,
+  defaultValue,
+}: SelectProps) {
   const [openSelect, setOpenSelect] = useState(false);
+  const [select, setSelect] = useState<string>(defaultValue ?? "");
 
   return (
     <div className={styles.select}>
@@ -20,13 +26,35 @@ export function Select({ onChange, options, placeholder, value }: SelectProps) {
         }`}
         onClick={() => setOpenSelect(prev => !prev)}
       >
-        <span>{value ? value : placeholder}</span>
+        <span>
+          {defaultValue ? defaultValue : select ? select : placeholder}
+        </span>
         <ChevronDown />
       </div>
       {openSelect && (
         <div className={styles.select__list}>
-          {options.map(option => (
-            <div key={crypto.randomUUID()}>{option}</div>
+          {options.map((option, index) => (
+            <button
+              style={{
+                borderRadius:
+                  options.length === 1
+                    ? "8px"
+                    : index === 0
+                    ? "8px 8px 0 0"
+                    : index + 1 === options.length
+                    ? "0 0 8px 8px"
+                    : "",
+              }}
+              className={`${option === select ? styles.active : ""}`}
+              key={crypto.randomUUID()}
+              onClick={() => {
+                setSelect(option);
+                onChange(option);
+                setOpenSelect(false);
+              }}
+            >
+              {option}
+            </button>
           ))}
         </div>
       )}
