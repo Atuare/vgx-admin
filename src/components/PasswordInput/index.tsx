9e9b42cdf69f81db9ab3f7f8a6019a7f45e514e6
Iury <iurysena74@gmail.com>
2023-08-28 20:28:49 -0300

    :sparkles: feat: create password input component

---
+++ src/components/PasswordInput/index.tsx
@@ -0,0 +1,34 @@
+import { VisibilityOff, VisibilityOn } from "@/assets/Icons";
+import { useState } from "react";
+import styles from "./PasswordInput.module.scss";
+
+interface PasswordInputProps {
+  name: string;
+  onChangePassword: (value: string) => void;
+}
+
+export function PasswordInput({ name, onChangePassword }: PasswordInputProps) {
+  const [showPassword, setShowPassword] = useState(false);
+
+  return (
+    <div className={styles.input}>
+      <label htmlFor={name}>{name}</label>
+      <div className={styles.inputContainer}>
+        <input
+          id={name}
+          onChange={e => onChangePassword(e.target.value)}
+          type={showPassword ? "text" : "password"}
+        />
+        {showPassword ? (
+          <button onClick={() => setShowPassword(false)} type="button">
+            <VisibilityOn />
+          </button>
+        ) : (
+          <button onClick={() => setShowPassword(true)} type="button">
+            <VisibilityOff />
+          </button>
+        )}
+      </div>
+    </div>
+  );
+}
