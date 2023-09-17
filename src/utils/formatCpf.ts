1db448db6566dff032197bab800c5585eec3243c
Iury <iurysena74@gmail.com>
2023-09-17 17:41:44 -0300

    :recycle: refactor: formatCpf function

--- src/utils/formatCpf.ts
+++ src/utils/formatCpf.ts
@@ -1,3 +1,8 @@
 export function formatCpf(cpf: string) {
-  return cpf.replace(/^(\d{3})(\d{3})(\d{3})(\d{2})$/, "$1.$2.$3-$4");
+  return cpf
+    .replace(/\D/g, "")
+    .replace(/(\d{3})(\d)/, "$1.$2")
+    .replace(/(\d{3})(\d)/, "$1.$2")
+    .replace(/(\d{3})(\d{1,2})/, "$1-$2")
+    .replace(/(-\d{2})\d+?$/, "$1");
 }
