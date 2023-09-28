202dd21adbfb0735f749bd0e047abfe841848f1d
Iury <iurysena74@gmail.com>
2023-09-27 21:58:35 -0300

    :sparkles: feat: add some validations for test create page

--- src/app/config/tests/create/page.tsx
+++ src/app/config/tests/create/page.tsx
@@ -7,13 +7,20 @@ import { TestCreateModal } from "@/components/Modals/TestCreateModal";
 import { Select } from "@/components/Select";
 import { TestsCreateTable } from "@/components/Tables/TestsCreateTable";
 import { TipTap } from "@/components/TipTap";
+import { testsCreateConfigSchema } from "@/schemas/configTestsSchema";
 import { useGetAllUnitsQuery } from "@/services/api/fetchApi";
+import { yupResolver } from "@hookform/resolvers/yup";
 import { Table } from "@tanstack/react-table";
 import { ReactNode, useState } from "react";
+import { Controller, useForm } from "react-hook-form";
 import styles from "./TestCreate.module.scss";
 
 export default function TestCreate() {
   const [table, setTable] = useState<Table<any>>();
+
+  const { control, handleSubmit } = useForm({
+    resolver: yupResolver(testsCreateConfigSchema),
+  });
 
   const { data: units } = useGetAllUnitsQuery({
     page: 1,
@@ -22,125 +29,217 @@ export default function TestCreate() {
     direction: "ASC",
   });
 
+  const handleCreateTest = (data: any) => {
+    // console.log(data);
+  };
+
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={handleSubmit(handleCreateTest)}>
       <h3 className={styles.form__title}>Dados prova</h3>
       <Container>
-        <DataInput name="Unidade/Site" required width="296px">
-          <Select
-            onChange={() => {}}
-            options={
-              units
-                ? units.units.map(value => {
-                    return { id: value.id, name: value.unitName };
-                  })
-                : []
-            }
-            placeholder="Selecione"
-          />
-        </DataInput>
-
-        <DataInput name="Tempo máx. prova" lightName="(minutos)" required>
-          <input
-            type="text"
-            pattern="\d*"
-            onChange={e => {
-              if (!e.target.validity.valid) {
-                e.target.value = "";
-                return;
-              }
-            }}
-            style={{ width: 128 }}
-          />
-        </DataInput>
+        <Controller
+          control={control}
+          name="unitId"
+          render={({ field: { onChange }, fieldState: { error } }) => (
+            <DataInput
+              name="Unidade/Site"
+              required
+              width="296px"
+              error={error?.message}
+            >
+              <Select
+                onChange={({ id }) => onChange(id)}
+                options={
+                  units
+                    ? units.units.map(value => {
+                        return { id: value.id, name: value.unitName };
+                      })
+                    : []
+                }
+                placeholder="Selecione"
+              />
+            </DataInput>
+          )}
+        />
+        <Controller
+          control={control}
+          name="maxTime"
+          render={({ field: { onChange }, fieldState: { error } }) => (
+            <DataInput
+              name="Tempo máx. prova"
+              lightName="(minutos)"
+              required
+              error={error?.message}
+            >
+              <input
+                type="text"
+                pattern="\d*"
+                onChange={e => {
+                  if (!e.target.validity.valid) {
+                    e.target.value = "";
+                    return;
+                  }
+                  onChange(e.target.value);
+                }}
+                style={{ width: 128 }}
+              />
+            </DataInput>
+          )}
+        />
       </Container>
 
       <Container>
-        <DataInput name="Total de questões português" required>
-          <input
-            type="text"
-            pattern="\d*"
-            onChange={e => {
-              if (!e.target.validity.valid) {
-                e.target.value = "";
-                return;
-              }
-            }}
-            style={{ width: 128 }}
-          />
-        </DataInput>
+        <Controller
+          control={control}
+          name="portTotal"
+          render={({ field: { onChange }, fieldState: { error } }) => (
+            <DataInput
+              name="Total de questões português"
+              required
+              error={error?.message}
+            >
+              <input
+                type="text"
+                pattern="\d*"
+                onChange={e => {
+                  if (!e.target.validity.valid) {
+                    e.target.value = "";
+                    return;
+                  }
+                  onChange(e.target.value);
+                }}
+                style={{ width: 128 }}
+              />
+            </DataInput>
+          )}
+        />
 
-        <DataInput name="Nota mín. aprovação" required>
-          <input
-            type="text"
-            pattern="\d*"
-            onChange={e => {
-              if (!e.target.validity.valid) {
-                e.target.value = "";
-                return;
-              }
-            }}
-            style={{ width: 128 }}
-          />
-        </DataInput>
+        <Controller
+          control={control}
+          name="portMinScore"
+          render={({ field: { onChange }, fieldState: { error } }) => (
+            <DataInput
+              name="Nota mín. aprovação"
+              required
+              error={error?.message}
+            >
+              <input
+                type="text"
+                pattern="\d*"
+                onChange={e => {
+                  if (!e.target.validity.valid) {
+                    e.target.value = "";
+                    return;
+                  }
+                  onChange(e.target.value);
+                }}
+                style={{ width: 128 }}
+              />
+            </DataInput>
+          )}
+        />
 
-        <DataInput name="Total de questões matemática" required>
-          <input
-            type="text"
-            pattern="\d*"
-            onChange={e => {
-              if (!e.target.validity.valid) {
-                e.target.value = "";
-                return;
-              }
-            }}
-            style={{ width: 128 }}
-          />
-        </DataInput>
+        <Controller
+          control={control}
+          name="matTotal"
+          render={({ field: { onChange }, fieldState: { error } }) => (
+            <DataInput
+              name="Total de questões matemática"
+              required
+              error={error?.message}
+            >
+              <input
+                type="text"
+                pattern="\d*"
+                onChange={e => {
+                  if (!e.target.validity.valid) {
+                    e.target.value = "";
+                    return;
+                  }
+                  onChange(e.target.value);
+                }}
+                style={{ width: 128 }}
+              />
+            </DataInput>
+          )}
+        />
 
-        <DataInput name="Nota mín. aprovação" required>
-          <input
-            type="text"
-            pattern="\d*"
-            onChange={e => {
-              if (!e.target.validity.valid) {
-                e.target.value = "";
-                return;
-              }
-            }}
-            style={{ width: 128 }}
-          />
-        </DataInput>
+        <Controller
+          control={control}
+          name="matMinScore"
+          render={({ field: { onChange }, fieldState: { error } }) => (
+            <DataInput
+              name="Nota mín. aprovação"
+              required
+              error={error?.message}
+            >
+              <input
+                type="text"
+                pattern="\d*"
+                onChange={e => {
+                  if (!e.target.validity.valid) {
+                    e.target.value = "";
+                    return;
+                  }
+                  onChange(e.target.value);
+                }}
+                style={{ width: 128 }}
+              />
+            </DataInput>
+          )}
+        />
       </Container>
 
       <Container>
-        <DataInput name="Total de questões noções informática" required>
-          <input
-            type="text"
-            pattern="\d*"
-            onChange={e => {
-              if (!e.target.validity.valid) {
-                e.target.value = "";
-                return;
-              }
-            }}
-            style={{ width: 128 }}
-          />
-        </DataInput>
+        <Controller
+          control={control}
+          name="compTotal"
+          render={({ field: { onChange }, fieldState: { error } }) => (
+            <DataInput
+              name="Total de questões noções informática"
+              required
+              error={error?.message}
+            >
+              <input
+                type="text"
+                pattern="\d*"
+                onChange={e => {
+                  if (!e.target.validity.valid) {
+                    e.target.value = "";
+                    return;
+                  }
+                  onChange(e.target.value);
+                }}
+                style={{ width: 128 }}
+              />
+            </DataInput>
+          )}
+        />
 
-        <DataInput name="Nota mín. aprovação" required>
-          <input
-            type="text"
-            pattern="\d*"
-            onChange={e => {
-              if (!e.target.validity.valid) {
-                e.target.value = "";
-                return;
-              }
-            }}
-            style={{ width: 128 }}
-          />
-        </DataInput>
+        <Controller
+          control={control}
+          name="compMinScore"
+          render={({ field: { onChange }, fieldState: { error } }) => (
+            <DataInput
+              name="Nota mín. aprovação"
+              required
+              error={error?.message}
+            >
+              <input
+                type="text"
+                pattern="\d*"
+                onChange={e => {
+                  if (!e.target.validity.valid) {
+                    e.target.value = "";
+                    return;
+                  }
+                  onChange(e.target.value);
+                }}
+                style={{ width: 128 }}
+              />
+            </DataInput>
+          )}
+        />
       </Container>
 
       <h3 className={styles.form__title}>Questões prova</h3>
@@ -168,14 +267,59 @@ export default function TestCreate() {
       <TestsCreateTable setTable={setTable} />
 
       <h3 className={styles.form__title}>Orientação para a prova</h3>
-      <TipTap getContentFromEditor={() => {}} />
+      <Controller
+        control={control}
+        name="orientationMessage"
+        render={({ field: { onChange }, fieldState: { error } }) => (
+          <div>
+            <TipTap
+              getContentFromEditor={content => {
+                if (content.content[0].content) {
+                  onChange(JSON.stringify(content));
+                }
+              }}
+            />
+            <p className={styles.error}>{error?.message}</p>
+          </div>
+        )}
+      />
 
       <h3 className={styles.form__title}>Mensagem final</h3>
       <h4 className={styles.form__subtitle}>Aprovação*</h4>
-      <TipTap getContentFromEditor={() => {}} />
+      <Controller
+        control={control}
+        name="aproveMessage"
+        render={({ field: { onChange }, fieldState: { error } }) => (
+          <div>
+            <TipTap
+              getContentFromEditor={content => {
+                if (content.content[0].content) {
+                  onChange(JSON.stringify(content));
+                }
+              }}
+            />
+            <p className={styles.error}>{error?.message}</p>
+          </div>
+        )}
+      />
 
       <h4 className={styles.form__subtitle}>Reprovação*</h4>
-      <TipTap getContentFromEditor={() => {}} />
+      <Controller
+        control={control}
+        name="disapprovedMessage"
+        render={({ field: { onChange }, fieldState: { error } }) => (
+          <div>
+            <TipTap
+              getContentFromEditor={content => {
+                if (content.content[0].content) {
+                  onChange(JSON.stringify(content));
+                }
+              }}
+            />
+            <p className={styles.error}>{error?.message}</p>
+          </div>
+        )}
+      />
 
       <div className={styles.form__buttons}>
         <Button buttonType="default" text="Cancelar" />
--- src/schemas/configTestsSchema.ts
+++ src/schemas/configTestsSchema.ts
@@ -1,5 +1,41 @@
 import * as yup from "yup";
 
+export const testsCreateConfigSchema = yup.object({
+  maxTime: yup
+    .number()
+    .typeError("Número inválid")
+    .required("Campo obrigatório"),
+  portTotal: yup
+    .number()
+    .typeError("Número inválido")
+    .required("Campo obrigatório"),
+  portMinScore: yup
+    .number()
+    .typeError("Número inválido")
+    .required("Campo obrigatório"),
+  matTotal: yup
+    .number()
+    .typeError("Número inválido")
+    .required("Campo obrigatório"),
+  matMinScore: yup
+    .number()
+    .typeError("Número inválido")
+    .required("Campo obrigatório"),
+  compTotal: yup
+    .number()
+    .typeError("Número inválido")
+    .required("Campo obrigatório"),
+  compMinScore: yup
+    .number()
+    .typeError("Número inválido")
+    .required("Campo obrigatório"),
+  orientationMessage: yup.string().required("Campo obrigatório"),
+  aproveMessage: yup.string().required("Campo obrigatório"),
+  disapprovedMessage: yup.string().required("Campo obrigatório"),
+  unitId: yup.string().required("Campo obrigatório"),
+  // questions: yup.array().required("Campo obrigatório"),
+});
+
 export const testsModalConfigSchema = yup
   .object({
     text: yup.string().required("Campo obrigatório"),
