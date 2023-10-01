69cb0f0060861051e21bda1d8f5bca54cb047c7c
Iury <iurysena74@gmail.com>
2023-10-01 09:32:47 -0300

    :sparkles: feat: create config inscriptions page

---
+++ src/app/config/inscriptions/page.tsx
@@ -0,0 +1,9 @@
+import styles from "./InscriptionsConfig.module.scss";
+
+export default function ConfigInscriptions() {
+  return (
+    <main className={styles.inscriptions}>
+      <h2>Pré requisitos para inscrição de candidatos</h2>
+    </main>
+  );
+}
