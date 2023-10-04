db5770ab215e226521e60a658cbd7e3c8b2acfb3
Iury <iurysena74@gmail.com>
2023-10-03 22:51:56 -0300

    :sparkles: feat: create formatCep and formatCurrency functions

--- src/utils/formatCurrency.ts
+++ src/utils/formatCurrency.ts
@@ -1,8 +1,13 @@
-export function formatCurrency(number: number) {
-  const formatter = new Intl.NumberFormat("pt-BR", {
+export function formatCurrency(value: number | string) {
+  let numericValue;
+  typeof value === "string"
+    ? (numericValue = value.replace(/\D/g, ""))
+    : (numericValue = value);
+
+  const formattedValue = (+numericValue / 100).toLocaleString("pt-BR", {
     style: "currency",
     currency: "BRL",
   });
 
-  return formatter.format(number);
+  return formattedValue;
 }
