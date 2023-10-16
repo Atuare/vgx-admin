be4df273863d8859d0f3fd3ca6bc4b0f4a9bf684
Iury <iurysena74@gmail.com>
2023-10-16 11:45:16 -0300

    :bug: fix: modal value from the document settings page was not reset when the modal is closed

--- src/components/Modals/DocumentsConfig/index.tsx
+++ src/components/Modals/DocumentsConfig/index.tsx
@@ -22,7 +22,13 @@ export function DocumentsConfigModal({
   create,
 }: DocumentsConfigModalProps) {
   const [open, setOpen] = useState(false);
-  const { control, handleSubmit } = useForm({
+  const {
+    control,
+    handleSubmit,
+    reset,
+    register,
+    formState: { errors },
+  } = useForm({
     resolver: yupResolver(documentConfigModalConfigSchema),
     defaultValues: {
       name: defaultValue?.name ?? "",
@@ -36,11 +42,18 @@ export function DocumentsConfigModal({
 
   function handleOnSave(data: any) {
     setOpen(false);
+    reset();
     handleOnSubmit(data);
   }
 
   return (
-    <Dialog.Root open={open} onOpenChange={setOpen}>
+    <Dialog.Root
+      open={open}
+      onOpenChange={val => {
+        setOpen(val);
+        if (!val) reset();
+      }}
+    >
       <Dialog.Trigger asChild>
         <span>
           {create ? (
@@ -54,129 +67,127 @@ export function DocumentsConfigModal({
           )}
         </span>
       </Dialog.Trigger>
-      <Dialog.Portal className={styles.modal}>
-        <Dialog.Overlay className={styles.modal__overlay} />
-        <Dialog.Content className={styles.modal__content}>
-          <form onSubmit={handleSubmit(handleOnSave)}>
-            <Dialog.Title className={styles.modal__title}>
-              {defaultValue?.name
-                ? `${defaultValue?.name} - Documento`
-                : "Novo Documento"}
-              <Dialog.Close asChild>
-                <span>
-                  <Close />
-                </span>
-              </Dialog.Close>
-            </Dialog.Title>
+      <Dialog.Portal>
+        <div className={styles.modal}>
+          <Dialog.Overlay className={styles.modal__overlay} />
+          <Dialog.Content className={styles.modal__content}>
+            <form onSubmit={handleSubmit(handleOnSave)}>
+              <Dialog.Title className={styles.modal__title}>
+                {defaultValue?.name
+                  ? `${defaultValue?.name} - Documento`
+                  : "Novo Documento"}
+                <Dialog.Close asChild>
+                  <span>
+                    <Close />
+                  </span>
+                </Dialog.Close>
+              </Dialog.Title>
 
-            <div className={styles.modal__content__form}>
-              <Controller
-                name="name"
-                control={control}
-                render={({ field: { onChange }, fieldState: { error } }) => (
-                  <div className={styles.modal__content__form__item}>
-                    <label htmlFor="document">Documento</label>
-                    <input
-                      id="document"
-                      type="text"
-                      defaultValue={defaultValue?.name}
-                      onChange={onChange}
+              <div className={styles.modal__content__form}>
+                <div className={styles.modal__content__form__item}>
+                  <label htmlFor="document">Documento</label>
+                  <input
+                    id="document"
+                    type="text"
+                    defaultValue={defaultValue?.name}
+                    {...register("name")}
+                  />
+                  <span className={styles.error}>
+                    {errors.name && errors.name.message}
+                  </span>
+                </div>
+
+                <Controller
+                  name="mandatory"
+                  control={control}
+                  render={({ field: { onChange } }) => (
+                    <Checkbox
+                      iconType="solid"
+                      value="Documento obrigatório"
+                      onChangeCheckbox={value => onChange(value)}
+                      style={{ fontWeight: "700", padding: 0 }}
                     />
-                    <span className={styles.error}>
-                      {error?.message && error.message}
-                    </span>
+                  )}
+                />
+
+                <div className={styles.modal__content__form__checkboxes}>
+                  <h1>Critérios de envio</h1>
+
+                  <div
+                    className={styles.modal__content__form__checkboxes__list}
+                  >
+                    <Controller
+                      name="man"
+                      control={control}
+                      render={({ field: { onChange } }) => (
+                        <Checkbox
+                          iconType="solid"
+                          value="Homens"
+                          onChangeCheckbox={value => onChange(value)}
+                          style={{ padding: 0, width: "auto" }}
+                        />
+                      )}
+                    />
+
+                    <Controller
+                      name="eighteenYears"
+                      control={control}
+                      render={({ field: { onChange } }) => (
+                        <Checkbox
+                          iconType="solid"
+                          value="Maior de 18 anos"
+                          onChangeCheckbox={value => onChange(value)}
+                          style={{ padding: 0, width: "auto" }}
+                        />
+                      )}
+                    />
+
+                    <Controller
+                      name="married"
+                      control={control}
+                      render={({ field: { onChange } }) => (
+                        <Checkbox
+                          iconType="solid"
+                          value="Casado(a)"
+                          onChangeCheckbox={value => onChange(value)}
+                          style={{ padding: 0, width: "auto" }}
+                        />
+                      )}
+                    />
+
+                    <Controller
+                      name="childrens"
+                      control={control}
+                      render={({ field: { onChange } }) => (
+                        <Checkbox
+                          iconType="solid"
+                          value="Ter filhos"
+                          onChangeCheckbox={value => onChange(value)}
+                          style={{ padding: 0, width: "auto" }}
+                        />
+                      )}
+                    />
                   </div>
-                )}
-              />
-
-              <Controller
-                name="mandatory"
-                control={control}
-                render={({ field: { onChange } }) => (
-                  <Checkbox
-                    iconType="solid"
-                    value="Documento obrigatório"
-                    onChangeCheckbox={value => onChange(value)}
-                    style={{ fontWeight: "700", padding: 0 }}
-                  />
-                )}
-              />
-
-              <div className={styles.modal__content__form__checkboxes}>
-                <h1>Critérios de envio</h1>
-
-                <div className={styles.modal__content__form__checkboxes__list}>
-                  <Controller
-                    name="man"
-                    control={control}
-                    render={({ field: { onChange } }) => (
-                      <Checkbox
-                        iconType="solid"
-                        value="Homens"
-                        onChangeCheckbox={value => onChange(value)}
-                        style={{ padding: 0, width: "auto" }}
-                      />
-                    )}
-                  />
-
-                  <Controller
-                    name="eighteenYears"
-                    control={control}
-                    render={({ field: { onChange } }) => (
-                      <Checkbox
-                        iconType="solid"
-                        value="Maior de 18 anos"
-                        onChangeCheckbox={value => onChange(value)}
-                        style={{ padding: 0, width: "auto" }}
-                      />
-                    )}
-                  />
-
-                  <Controller
-                    name="married"
-                    control={control}
-                    render={({ field: { onChange } }) => (
-                      <Checkbox
-                        iconType="solid"
-                        value="Casado(a)"
-                        onChangeCheckbox={value => onChange(value)}
-                        style={{ padding: 0, width: "auto" }}
-                      />
-                    )}
-                  />
-
-                  <Controller
-                    name="childrens"
-                    control={control}
-                    render={({ field: { onChange } }) => (
-                      <Checkbox
-                        iconType="solid"
-                        value="Ter filhos"
-                        onChangeCheckbox={value => onChange(value)}
-                        style={{ padding: 0, width: "auto" }}
-                      />
-                    )}
-                  />
                 </div>
               </div>
-            </div>
 
-            <div
-              style={{
-                display: "flex",
-                gap: 10,
-                justifyContent: "flex-end",
-                padding: 10,
-              }}
-            >
-              <Dialog.Close asChild>
-                <Button text="Cancelar" buttonType="default" type="button" />
-              </Dialog.Close>
+              <div
+                style={{
+                  display: "flex",
+                  gap: 10,
+                  justifyContent: "flex-end",
+                  padding: 10,
+                }}
+              >
+                <Dialog.Close asChild>
+                  <Button text="Cancelar" buttonType="default" type="button" />
+                </Dialog.Close>
 
-              <Button text="Salvar" buttonType="primary" type="submit" />
-            </div>
-          </form>
-        </Dialog.Content>
+                <Button text="Salvar" buttonType="primary" type="submit" />
+              </div>
+            </form>
+          </Dialog.Content>
+        </div>
       </Dialog.Portal>
     </Dialog.Root>
   );
